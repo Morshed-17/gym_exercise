464e453f1bb63a7360c5d87d6ea816b868ed4a8c
Morshed-17 <morshedmto@gmail.com>
2023-12-09 23:30:30 +0600

    Exercise detail component added

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,9 +1,35 @@
-
+import { Box } from "@mui/material";
+import Detail from "../components/Detail";
+import SimilarExercises from "../components/SimilarExercises";
+import ExerciseVideos from "../components/ExerciseVideos";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchData, exerciseOptions } from "../utils/fetchData";
 
 const ExerciseDetail = () => {
-  return (
-    <div>ExerciseDetail</div>
-  )
-}
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
 
-export default ExerciseDetail
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl =
+        "https://youtube-search-and-download.p.rapidapi.com";
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions)
+
+      setExerciseDetail(exerciseDetailData);
+    };
+    fetchExercisesData();
+  }, [id]);
+
+  return (
+    <Box>
+      <Detail exerciseDetail={exerciseDetail}/>
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
+  );
+};
+
+export default ExerciseDetail;
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -5,7 +5,19 @@ export const exerciseOptions = {
       'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
-  };
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  url: 'https://youtube-search-and-download.p.rapidapi.com/channel/about',
+  params: {
+    id: 'UCE_M8A5yxnLfW0KghEeajjw'
+  },
+  headers: {
+    'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+  }
+};
 
 
 export const fetchData = async (url, options) => {
